d859e8ce47b8f719c872fe58f287e8fa4afdb945
mwhite <mattp.white95@gmail.com>
2021-01-02 16:23:47 -0800

    Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.scss';
 function App() {
   return (
           <div className='Home'>
-              <h1>Hey, I'm Matt.</h1>
+            <h1>Hey, I'm Matt.</h1>
+            <p>Did you know? Flights are cheapest within 2 weeks of departure. Make sure you wait till then next time you want to fly on the cheap!</p>
           </div>
   );
 }
